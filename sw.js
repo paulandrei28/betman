20806b9861e9b5cb234e9cf76724f5ef54e5b978
paulandrei28 <paulandreis@yahoo.com>
2026-09-03 23:46:22 +0300

    web app using game analyst data

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'betman-shell-v9';
+const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches.keys().then((keys) => Promise.all(
+			keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)),
+		)),
+	);
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') return;
+
+	const isDataRequest = new URL(event.request.url).pathname.includes('/data/');
+	if (isDataRequest) {
+		event.respondWith(
+			fetch(event.request, { cache: 'no-cache' })
+				.then((response) => {
+					const copy = response.clone();
+					caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+					return response;
+				})
+				.catch(() => caches.match(event.request)),
+		);
+		return;
+	}
+
+	event.respondWith(
+		caches.match(event.request)
+			.then((cached) => cached || fetch(event.request))
+			.catch(() => caches.match('./index.html')),
+	);
+});
