66e47f9e5fa87759c7668631ef9a4171e8c34464
paulandrei28 <paulandreis@yahoo.com>
2026-09-04 10:20:32 +0300

    fix empty reports issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'betman-shell-v9';
+const CACHE = 'betman-shell-v10';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
