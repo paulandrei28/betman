9f018d0d88d39c1804340911e25ca2653aa56385
paulandrei28 <paulandreis@yahoo.com>
2026-09-04 10:54:07 +0300

    improve report readability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'betman-shell-v10';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'betman-shell-v11';
+const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
 	event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
@@ -33,8 +33,12 @@ self.addEventListener('fetch', (event) => {
 	}
 
 	event.respondWith(
-		caches.match(event.request)
-			.then((cached) => cached || fetch(event.request))
-			.catch(() => caches.match('./index.html')),
+			fetch(event.request)
+				.then((response) => {
+					const copy = response.clone();
+					caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+					return response;
+				})
+				.catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html'))),
 	);
 });
